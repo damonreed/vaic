d7ca3c4cbe7013ea75e532ef9b491d64efc9a9c9
Damon Reed <dreed@nomadia.org>
2024-02-19 18:11:25 -0600

    Testing the storytelling ability of Gemini.

--- test.py
+++ test.py
@@ -5,5 +5,8 @@ text_prompt = """
 Develop a gritty fantasy plot outline for a graphic novel.  There is a slight steampunk aspect to the world.
 The protagonist is a sly rogue who is a master of disguise.  He is a bit of a loner and has a dark past.
 His love interest is a upright paladin who is a bit naive.  She is a bit of a zealot and has a strong sense of justice.
+One antagonist is a former lover of the protagonist who is a rival rogue.  She is a bit of a femme fatale and has a very dark past.
+The protagonist has pledged his loyalty to a powerful firemage who runs a cirle of adventurers.  
+The mage is supported by a priest of the light who struggles with her own inner darkness.
 """
 print(text_response(text_prompt))
